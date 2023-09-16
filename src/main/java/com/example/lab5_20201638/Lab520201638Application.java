2ab1cb12240e08fd2a2f3ffcb3c73165e81f8bf0
David Llanos <david.llanos@pucp.edu.pe>
2023-09-15 19:33:31 -0500

    first commit

---
+++ src/main/java/com/example/lab5_20201638/Lab520201638Application.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20201638;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab520201638Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab520201638Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab5_20201638/Lab520201638ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20201638;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520201638ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
